1cd4b92f5a471cd8b69a0be0213f6076a5d87065
laksamanaap <laksamana.arya1412@gmail.com>
2023-09-04 23:31:23 +0700

    tes commit

---
+++ main.js
@@ -0,0 +1,11 @@
+
+// alert("tes");
+console.log("testing init")
+
+function startGame() {}
+function increaseTime() {}
+function createCorruptor() {}
+function getRandomCorruptor() { }
+function catchCorruptor() { }
+function addCorruptor() { }
+function increaseScore() {}
