d66944fca8d111c884b556505b1a1c951f81570e
laksamanaap <laksamana.arya1412@gmail.com>
2023-09-06 00:11:16 +0700

    feat : setup functionality

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const start = document.getElementById("start");
 const screen = document.getElementsByClassName("screen");
-const choose_corruptor = document.querySelectorAll(".corruptor-list");
-
-// console.log(choose_corruptor);
+const choose_corruptor = document.querySelectorAll(".choose-corruptor-btn");
+const score = [];
+const seconds = [];
 
 // Swipe Up
 start.addEventListener("click", () => {
@@ -10,18 +10,33 @@ start.addEventListener("click", () => {
     // console.log("click");
 })
 
-choose_corruptor.forEach(btn => {
-    btn.addEventListener("click", () => {
-        const img = btn.querySelector('img')
-        const src = img.getAttribute('src')
-        const alt = img.getAttribute('alt')
-        selected_corruptor = { src, alt } // Destructure
-        screen[1].classList.add('up')
-        // console.log(selected_corruptor);
-        document.querySelector(".tes").innerHTML = selected_corruptor.alt;
-    })
+// choose_corruptor.forEach(btn => {
+//     btn.addEventListener("click", () => {
+//         const img = btn.querySelector('img')
+//         const src = img.getAttribute('src')
+//         const alt = img.getAttribute('alt')
+//         selected_corruptor = { src, alt } // Destructure
+//         screen[1].classList.add('up')
+//         // console.log(src.length);
+//     })
+// })
 
-})
+for (let i = 0; i < choose_corruptor.length; i++) {
+    const btn = choose_corruptor[i];
+    console.log(btn);
+
+  btn.addEventListener("click", () => {
+    const img = btn.querySelector('img');
+    const src = img.getAttribute('src');
+    const alt = img.getAttribute('alt');
+    const selected_corruptor = { src, alt }; // Destructure
+    screen[1].classList.add('up');
+      
+      document.querySelector(".tes").innerHTML = selected_corruptor.alt;
+  });
+}
+
+
 
 
 function startGame() {}
