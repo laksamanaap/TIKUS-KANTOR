6e9782077e98a878849caf2e5db70ee45e7edaa4
laksamanaap <laksamana.arya1412@gmail.com>
2023-09-05 23:49:43 +0700

    feat : setup functionality

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const start = document.getElementById("start");
 const screen = document.getElementsByClassName("screen");
-const choose_corruptor = document.querySelectorAll("corruptor-list");
+const choose_corruptor = document.querySelectorAll(".corruptor-list");
 
+// console.log(choose_corruptor);
 
 // Swipe Up
 start.addEventListener("click", () => {
     screen[0].classList.add("up");
-    console.log("click");
+    // console.log("click");
 })
 
 choose_corruptor.forEach(btn => {
@@ -14,10 +15,12 @@ choose_corruptor.forEach(btn => {
         const img = btn.querySelector('img')
         const src = img.getAttribute('src')
         const alt = img.getAttribute('alt')
-        let selected_corruptor = { src, alt }
-        selected_corruptor;
-        screens[1].classList.add('up')
+        selected_corruptor = { src, alt } // Destructure
+        screen[1].classList.add('up')
+        // console.log(selected_corruptor);
+        document.querySelector(".tes").innerHTML = selected_corruptor.alt;
     })
+
 })
 
 
