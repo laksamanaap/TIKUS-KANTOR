236ab995b0b78e15282138a6e238f9e171398324
laksamanaap <laksamana.arya1412@gmail.com>
2023-09-06 22:08:44 +0700

    feat : start and time functionality

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const start = document.getElementById("start");
 const screen = document.getElementsByClassName("screen");
 const choose_corruptor = document.querySelectorAll(".choose-corruptor-btn");
-const score = [];
-const seconds = [];
+const timeFunc = document.getElementById("time");
+const scoreFunc = document.getElementById("score");
+let score = 0;
+let seconds = 0;
+let selected_corruptor = {};
 
 // Swipe Up
 start.addEventListener("click", () => {
@@ -10,37 +13,47 @@ start.addEventListener("click", () => {
     // console.log("click");
 })
 
-// choose_corruptor.forEach(btn => {
-//     btn.addEventListener("click", () => {
-//         const img = btn.querySelector('img')
-//         const src = img.getAttribute('src')
-//         const alt = img.getAttribute('alt')
-//         selected_corruptor = { src, alt } // Destructure
-//         screen[1].classList.add('up')
-//         // console.log(src.length);
-//     })
-// })
-
-for (let i = 0; i < choose_corruptor.length; i++) {
-    const btn = choose_corruptor[i];
-    console.log(btn);
-
+choose_corruptor.forEach((btn, index) => {
   btn.addEventListener("click", () => {
-    const img = btn.querySelector('img');
-    const src = img.getAttribute('src');
-    const alt = img.getAttribute('alt');
-    const selected_corruptor = { src, alt }; // Destructure
-    screen[1].classList.add('up');
+        const img = btn.querySelector('img')
+        const src = img.getAttribute('src')
+        const alt = img.getAttribute('alt')
+        selected_corruptor = { src, alt } // Destructure
+        screen[1].classList.add('up')
+        startGame();
+        // console.log(index);
+        // document.querySelector(".tes").innerHTML = selected_corruptor.alt[index];
+    })
+})
+
+// for (let i = 0; i < choose_corruptor.length; i++) {
+//     const btn = choose_corruptor[i];
+//     console.log(btn);
+
+//   btn.addEventListener("click", () => {
+//     const img = btn.querySelector('img');
+//     const src = img.getAttribute('src');
+//     const alt = img.getAttribute('alt');
+//     const selected_corruptor = { src, alt }; // Destructure
+//     screen[1].classList.add('up');
       
-      document.querySelector(".tes").innerHTML = selected_corruptor.alt;
-  });
+//   });
+// }
+
+function startGame() {
+  // document.querySelector(".tes").innerHTML = 'Game Started';
+  setInterval(increaseTime,1000);
 }
 
+function increaseTime() {
+  let minute = Math.floor(seconds / 60);
+  let second = seconds % 60;
+  minute = minute < 10 ? `0${minute}` : minute;
+  second = second < 10 ? `0${second}` : second;
+  timeFunc.innerHTML = `Time: ${minute}:${second}` // Template Literals
+  seconds++
+}
 
-
-
-function startGame() {}
-function increaseTime() {}
 function createCorruptor() {}
 function getRandomCorruptor() { }
 function catchCorruptor() { }
